b8a785770a1e8b101a5f45553df9bb91d62e2794
adelchik <adel.sabirova@yahoo.com>
2018-09-30 02:36:29 -0700

    Updated with sensor readings

--- weather-sensor/__init__.py
+++ weather-sensor/__init__.py
@@ -5,22 +5,32 @@ Measures weather sensors on Raspberry PI and sends the data to Outlyer every
 @author Tagir Sabirov
 """
 import requests
-import psutil
 import time
-import json
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+# Import bme280 script
+import bme280
+
+# Software SPI configuration for ADC:
+CLK  = 18
+MISO = 23
+MOSI = 24
+CS   = 25
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 SLEEP_TIME = 30
 OUTLYER_API_KEY = ""
 OUTLYER_API_URL = "https://api2.outlyer.com/v2/accounts/tagir/series"
 
-def send_to_outlyer(metric:str , reading:float):
+def send_to_outlyer(data):
     """
     Sends a reading to Outlyer
 
-    :param metric:      The name of the metric in Outlyer
-    :param reading:     The reading value to send
+    :param data:      Array of samples
     """
-    print(f"recieved reading for {metric}: {reading}")
 
     headers = {
         'Authorization': 'Bearer ' + OUTLYER_API_KEY,
@@ -28,31 +38,70 @@ def send_to_outlyer(metric:str , reading:float):
         'Accepts': 'application/json'
     }
 
-    data = {
-        'samples': [
-            {
-                "host": "raspberry-pi",
-                "labels": {},
-                "name": metric,
-                "timestamp": int(round(time.time() * 1000)),
-                "ttl": (SLEEP_TIME * 2),
-                "type": "gauge",
-                "value": reading
-            }
-        ]
-    }
-
     resp = requests.post(OUTLYER_API_URL, json=data, headers=headers)
 
     if resp.status_code != 200:
-        print(f"ERROR POSTING DATA TO OUTLYER: {resp.text}")
+        print("ERROR POSTING DATA TO OUTLYER: " +  {resp.text})
 
 if __name__ == '__main__':
     while True:
-        cpu_usage = psutil.cpu_percent()
-        send_to_outlyer("sys.cpu.pct", cpu_usage)
+
+        # Read all the ADC channel 1.
+        adc_0 = mcp.read_adc(0)
+        # 0.4V (125) = 0m/s -> 2.0v (625) = 32.4m/s
+        # 500 steps, each step = 0.0648m/s
+        wind_speed = (adc_0 - 125) * 0.0648
+
+        # Read BME280 Readings
+        temperature, pressure, humidity = bme280.readBME280All()
+
+        # Print readings
+        print("Wind Speed: " + str(wind_speed) + "m/s | Pressure: " + str(pressure) + "hPa | Temperature: "
+              + str(temperature) + "C | Humidity: " + str(humidity))
+
+        data = {
+            'samples': [
+                {
+                    "host": "raspberry-pi",
+                    "labels": {},
+                    "name": "pi.wind_speed",
+                    "timestamp": int(round(time.time() * 1000)),
+                    "ttl": (SLEEP_TIME * 2),
+                    "type": "gauge",
+                    "value": wind_speed
+                },
+                {
+                    "host": "raspberry-pi",
+                    "labels": {},
+                    "name": "pi.pressure",
+                    "timestamp": int(round(time.time() * 1000)),
+                    "ttl": (SLEEP_TIME * 2),
+                    "type": "gauge",
+                    "value": pressure
+                },
+                {
+                    "host": "raspberry-pi",
+                    "labels": {},
+                    "name": "pi.temperature",
+                    "timestamp": int(round(time.time() * 1000)),
+                    "ttl": (SLEEP_TIME * 2),
+                    "type": "gauge",
+                    "value": temperature
+                },
+                {
+                    "host": "raspberry-pi",
+                    "labels": {},
+                    "name": "pi.humidity",
+                    "timestamp": int(round(time.time() * 1000)),
+                    "ttl": (SLEEP_TIME * 2),
+                    "type": "gauge",
+                    "value": humidity
+                },
+
+            ]
+        }
+
+        send_to_outlyer(data)
 
         # Wait
         time.sleep(SLEEP_TIME)
-
-
